87f541ac4265bf89c536d076c0e8f8faf5a1d281
joshuakim0724 <joshuakim0724@gmail.com>
2018-02-27 01:10:43 -0600

    Added Methods to Time Class

--- src/com/example/Time.java
+++ src/com/example/Time.java
@@ -4,9 +4,13 @@ public class Time {
 
     private int hours;
     private int minutes;
+    private int day;
 
     private static final int OPENING_HOURS = 6;
     private static final int CLOSING_HOURS = 22;
+    private static final int FINAL_HOUR = 24;
+    private static final int FINAL_MINUTE = 60;
+    private static final int MARKET_TRIP_TIME = 1;
 
     public int getHours() {
         return hours;
@@ -16,34 +20,57 @@ public class Time {
         return minutes;
     }
 
-    public void setHours(int hours) {
-        this.hours = hours;
+    public int getDay() {
+        return day;
     }
 
     public void addHours(int hours) {
         int addedHours = this.hours + hours;
 
-        if (addedHours > 24) {
-            addedHours = addedHours - 24;
+        while (addedHours > FINAL_HOUR) {
+            addedHours = addedHours - FINAL_HOUR;
+            addDay();
         }
         this.hours = addedHours;
-    }
-
-    public void setMinutes(int minutes) {
-        this.minutes = minutes;
     }
 
     public void addMinutes(int minutes) {
         int addedMinutes = this.minutes + minutes;
 
-        if (addedMinutes > 60) {
-            addedMinutes = addedMinutes - 60;
+        while (addedMinutes >= FINAL_MINUTE) {
+            addedMinutes = addedMinutes - FINAL_MINUTE;
             this.hours ++;
         }
         this.minutes = addedMinutes;
     }
 
+    private void addDay() {
+        day++;
+    }
+
+    public StringBuffer displayTime() {
+        StringBuffer timeOutput = new StringBuffer();
+
+        timeOutput.append(SimulationConstants.TIME_IS + hours + ":" + minutes);
+        timeOutput.append("\n");
+        timeOutput.append(SimulationConstants.DAY_PASSED + day);
+
+        return timeOutput;
+    }
+
     public boolean isResturantOpen() {
         return OPENING_HOURS < hours && hours < CLOSING_HOURS;
+    }
+
+    public boolean marketTrip() {
+        int addedHours = this.hours + MARKET_TRIP_TIME;
+
+        if (addedHours > FINAL_HOUR) {
+            hours = addedHours - FINAL_HOUR;
+            return true;
+        }
+        hours = addedHours;
+
+        return false;
     }
 }
